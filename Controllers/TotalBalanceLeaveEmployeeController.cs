9fce63197f7b93b1a5009a464662013015d6d293
Gajanan Kadam <Gajanankadam13@gmail.com>
2022-08-27 11:13:33 +0530

    All final changes

--- Controllers/TotalBalanceLeaveEmployeeController.cs
+++ Controllers/TotalBalanceLeaveEmployeeController.cs
@@ -31,17 +31,20 @@ namespace EmployeePortal.Controllers
             CT_AddBalanceLeave objExp = new CT_AddBalanceLeave();
             List<CT_AddBalanceLeave> listLeaveBal = new List<CT_AddBalanceLeave>();
             int EmployeePKID = Convert.ToInt32(Session["EmployeePKID"]);
+            
             listLeaveBal = objdl_BalLeave.GetCurrentEmployeeBalanceLeave(EmployeePKID);
-
-            ViewBag.BalLeavePKID = listLeaveBal[0].BalLeavePKID;
-            ViewBag.EmployeePKID = listLeaveBal[0].EmployeePKID;
-            ViewBag.EmployeeName = listLeaveBal[0].EmployeeName;
-            ViewBag.LossOfPay = listLeaveBal[0].LossOfPay;
-            ViewBag.CasualLeave = listLeaveBal[0].CasualLeave;
-            ViewBag.CompOff = listLeaveBal[0].CompOff;
-            ViewBag.SickLeave = listLeaveBal[0].SickLeave;
-            ViewBag.RecreationalLeave = listLeaveBal[0].RecreationalLeave;
-            ViewBag.CreatedOnFormat = listLeaveBal[0].CreatedOnFormat;
+            if (listLeaveBal.Count>0)
+            {
+                ViewBag.BalLeavePKID = listLeaveBal[0].BalLeavePKID;
+                ViewBag.EmployeePKID = listLeaveBal[0].EmployeePKID;
+                ViewBag.EmployeeName = listLeaveBal[0].EmployeeName;
+                ViewBag.LossOfPay = listLeaveBal[0].LossOfPay;
+                ViewBag.CasualLeave = listLeaveBal[0].CasualLeave;
+                ViewBag.CompOff = listLeaveBal[0].CompOff;
+                ViewBag.SickLeave = listLeaveBal[0].SickLeave;
+                ViewBag.RecreationalLeave = listLeaveBal[0].RecreationalLeave;
+                ViewBag.CreatedOnFormat = listLeaveBal[0].CreatedOnFormat;
+            }
 
             result = this.Json(JsonConvert.SerializeObject(listLeaveBal), JsonRequestBehavior.AllowGet);
             return result;
